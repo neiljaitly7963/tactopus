4dd2c192c81a3d56737a2219c48d74cc590a4f2a
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-09-02 21:09:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      data : []
+    }
+  }
+
+  componentDidMount(){
+		fetch('https://picsum.photos/list')
+		.then(response => response.json())
+		.then(data => {
+      let newData = data.splice(0, 2)
+      this.setState({data:newData})
+    });
+  }
+  
+   download = (url) => {
+    var element = document.createElement("a");
+    var file = new Blob(
+      [
+        url
+      ],
+      { type: "image/*" }
+    );
+    element.href = URL.createObjectURL(file);
+    element.download = "image.jpg";
+    element.click();
+  };
+
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+      {
+        this.state.data.map((image) => {
+          return(
+            <div className="box">
+            <img src={`${image.post_url}/download`} />
+            <div><button onClick={() => this.download(`${image.post_url}/download`)}>Download</button> <span>{image.author}</span></div>
+          </div>
+          )
+        })
+      }
+
       </header>
     </div>
   );
+  }
 }
 
 export default App;
